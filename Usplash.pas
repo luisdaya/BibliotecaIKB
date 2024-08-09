dc53eff5fa112cda0c81bd19d6bab78d444a0acc
Luís Gaspar <53667138+luisdaya@users.noreply.github.com>
2024-08-09 18:12:15 +0100

    Primeiro Commite

---
+++ Usplash.pas
@@ -0,0 +1,29 @@
+unit Usplash;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts;
+
+type
+  TForm1 = class(TForm)
+    Rectangle1: TRectangle;
+    Z: TLayout;
+    Image1: TImage;
+    Label1: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
